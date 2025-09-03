cf09578ea2307734c22707800189ffe7f7b20c4f
webdevelopersinkenya <mirah.gabriel1969@gmail.com>
2025-09-03 12:43:20 +0300

    wdd131

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,8 @@
+ 
+        // Set the current year in the footer
+        document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+        // Set the last modified date of the document
+        document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+    
+
